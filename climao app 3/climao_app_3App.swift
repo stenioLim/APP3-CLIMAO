cefa27dee5f02867a07a6fcb0d83ce109189843c
stenio bomfim <stenio_lima2016@hotmail.com>
2024-06-12 10:57:58 -0300

    Initial Commit

---
+++ climao app 3/climao_app_3App.swift
@@ -0,0 +1,17 @@
+//
+//  climao_app_3App.swift
+//  climao app 3
+//
+//  Created by stenio Lima on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct climao_app_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
